3941c99bd3de0908fe4f5bce2e50954611c06315
IvanGodPro24 <vanyanep@gmail.com>
2025-09-20 10:34:29 +0300

    add lab2

--- lab1/nightlight/app.py
+++ lab1/nightlight/app.py
@@ -1,2 +1,11 @@
+import time
+from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
 from counterfit_connection import CounterFitConnection
 CounterFitConnection.init('127.0.0.1', 5001)
+
+light_sensor = GroveLightSensor(49)
+
+while True:
+    light = light_sensor.light
+    print('Light level:', light)
+    time.sleep(1)
