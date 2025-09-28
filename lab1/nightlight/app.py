24726e5763a5c3883724f3185224ceb8d433d22f
IvanGodPro24 <vanyanep@gmail.com>
2025-09-28 09:15:22 +0300

    Add lab4

--- lab1/nightlight/app.py
+++ lab1/nightlight/app.py
@@ -1,4 +1,6 @@
+import paho.mqtt.client as mqtt
 import time
+import json
 from counterfit_shims_grove.grove_led import GroveLed
 from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
 from counterfit_connection import CounterFitConnection
@@ -7,11 +9,36 @@ CounterFitConnection.init('127.0.0.1', 5001)
 light_sensor = GroveLightSensor(49)
 led = GroveLed(50)
 
+id = 'e5402df2-7b4e-42bf-babe-1e64fb26ce45'
+client_name = id + 'nightlight_client'
+client_telemetry_topic = id + '/telemetry'
+server_command_topic = id + '/commands'
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_name)
+mqtt_client.connect('test.mosquitto.org')
+
+mqtt_client.loop_start()
+
+def handle_command(client, userdata, message):
+    payload = json.loads(message.payload.decode())
+    print("Message received:", payload)
+    
+    if payload['led_on']:
+        led.on()
+    else:
+        led.off()
+
+mqtt_client.subscribe(server_command_topic)
+mqtt_client.on_message = handle_command
+
+print("MQTT connected!")
+
 while True:
     light = light_sensor.light
-    print('Light level:', light)
-    if light < 300:
-        led.on()
-    else: 
-        led.off()
-    time.sleep(1)
+    telemetry = json.dumps({'light': light})
+
+    print("Sending telemetry:", telemetry)
+
+    mqtt_client.publish(client_telemetry_topic, telemetry)
+
+    time.sleep(5)
